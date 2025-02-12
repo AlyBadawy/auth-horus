c2681b7ee2037bb7fd45ecb1cbe7c3773af3f8f3
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-02-11 23:06:49 -0500

    [Feat] Role model and Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_12_030444) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_12_035242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
+
+  create_table "roles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "role_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_name"], name: "index_roles_on_role_name", unique: true
+  end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email_address", null: false
---
+++ spec/requests/roles_spec.rb
@@ -0,0 +1,108 @@
+require "rails_helper"
+
+RSpec.describe "/roles", type: :request do
+  let(:valid_attributes) {
+    { role_name: "Admin" }
+  }
+
+  let(:invalid_attributes) {
+    { role_name: nil }
+  }
+
+  let(:valid_headers) {
+    {}
+  }
+
+  describe "GET /index" do
+    it "renders a successful response" do
+      Role.create! valid_attributes
+      get roles_url, headers: valid_headers, as: :json
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET /show" do
+    it "renders a successful response" do
+      role = Role.create! valid_attributes
+      get role_url(role), as: :json
+      expect(response).to be_successful
+    end
+  end
+
+  describe "POST /create" do
+    context "with valid parameters" do
+      it "creates a new Role" do
+        expect {
+          post roles_url,
+               params: { role: valid_attributes }, headers: valid_headers, as: :json
+        }.to change(Role, :count).by(1)
+      end
+
+      it "renders a JSON response with the new role" do
+        post roles_url,
+             params: { role: valid_attributes }, headers: valid_headers, as: :json
+        expect(response).to have_http_status(:created)
+        expect(response.content_type).to match(a_string_including("application/json"))
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not create a new Role" do
+        expect {
+          post roles_url,
+               params: { role: invalid_attributes }, as: :json
+        }.not_to change(Role, :count)
+      end
+
+      it "renders a JSON response with errors for the new role" do
+        post roles_url,
+             params: { role: invalid_attributes }, headers: valid_headers, as: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to match(a_string_including("application/json"))
+      end
+    end
+  end
+
+  describe "PATCH /update" do
+    context "with valid parameters" do
+      let(:new_attributes) {
+        { role_name: "User" }
+      }
+
+      it "updates the requested role" do
+        role = Role.create! valid_attributes
+        patch role_url(role),
+              params: { role: new_attributes }, headers: valid_headers, as: :json
+        role.reload
+        expect(role.role_name).to eq("User")
+      end
+
+      it "renders a JSON response with the role" do
+        role = Role.create! valid_attributes
+        patch role_url(role),
+              params: { role: new_attributes }, headers: valid_headers, as: :json
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to match(a_string_including("application/json"))
+      end
+    end
+
+    context "with invalid parameters" do
+      it "renders a JSON response with errors for the role" do
+        role = Role.create! valid_attributes
+        patch role_url(role),
+              params: { role: invalid_attributes }, headers: valid_headers, as: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to match(a_string_including("application/json"))
+      end
+    end
+  end
+
+  describe "DELETE /destroy" do
+    it "destroys the requested role" do
+      role = Role.create! valid_attributes
+      expect {
+        delete role_url(role), headers: valid_headers, as: :json
+      }.to change(Role, :count).by(-1)
+    end
+  end
+end
