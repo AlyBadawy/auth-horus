268b9fa515969704bedb96dcb6aab6a724022026
Aly Badawy <1198568+AlyBadawy@users.noreply.github.com>
2025-02-13 22:50:27 -0500

    [Fix] Use resources for Session routes

--- spec/routing/sessions_routing_spec.rb
+++ spec/routing/sessions_routing_spec.rb
@@ -3,23 +3,29 @@ require "rails_helper"
 RSpec.describe SessionsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/identity/").to route_to("sessions#index")
+      expect(get: "/sessions").to route_to("sessions#index")
     end
 
     it "routes to #show" do
-      expect(get: "/identity/1").to route_to("sessions#show", id: "1")
+      expect(get: "/session").to route_to("sessions#show")
+      expect(get: "/sessions/1").to route_to("sessions#show", id: "1")
     end
 
-    it "routes to #sign_in" do
-      expect(post: "/identity/sign_in").to route_to("sessions#sign_in")
+    it "routes to #create (sign_in)" do
+      expect(post: "/session").to route_to("sessions#create")
     end
 
-    it "routes to #refresh via PUT" do
-      expect(put: "/identity/refresh").to route_to("sessions#refresh")
+    it "routes to #update (refresh) via PUT" do
+      expect(put: "/session").to route_to("sessions#update")
     end
 
-    it "routes to #sign_out" do
-      expect(delete: "/identity/sign_out").to route_to("sessions#sign_out")
+    it "routes to #update (refresh) via PATCH" do
+      expect(patch: "/session").to route_to("sessions#update")
+    end
+
+    it "routes to #destroy" do
+      expect(delete: "/session").to route_to("sessions#destroy")
+      expect(delete: "/sessions/1").to route_to("sessions#destroy", id: "1")
     end
   end
 end
